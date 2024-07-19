cb1cd863b30e472965afc74c3d309fbf3b8f19af
Raquel Cabanyes <raquel.cabanyes@grupoonetec.com>
2024-07-19 12:47:18 +0200

    add header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { faPhone } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'personal-portfolio';
+  public faPhone = faPhone;
 }
