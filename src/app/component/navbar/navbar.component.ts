56473b9a7e999fb9228f2847952c76e998034287
Raquel Cabanyes <raquel.cabanyes@grupoonetec.com>
2024-08-07 11:09:16 +0200

    change navbar styles for mobile

--- src/app/component/navbar/navbar.component.ts
+++ src/app/component/navbar/navbar.component.ts
@@ -1,14 +1,24 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { faPhone } from '@fortawesome/free-solid-svg-icons';
+import { faPhone, faBars } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [FontAwesomeModule],
+  imports: [FontAwesomeModule, CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss',
 })
 export class NavbarComponent {
   public faPhone = faPhone;
+  public faBars = faBars;
+  public isActive = false;
+
+  public showmenu() {
+    if(this.isActive || window.innerWidth > 768) {
+        return true;
+    }
+    return false;
+  }
 }
