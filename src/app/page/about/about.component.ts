3611de7dfbc99e41b8910bbfed6e182824491011
Raquel Cabanyes <raquel.cabanyes@grupoonetec.com>
2024-08-07 14:48:00 +0200

    added content

--- src/app/page/about/about.component.ts
+++ src/app/page/about/about.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss',
 })
-export class AboutComponent {}
+export class AboutComponent {
+  public iconLikedin = '../../../assets/icons/linkedin.svg';
+  public urlLikedin = 'https://es.linkedin.com/in/raquelcabanyes';
+}
