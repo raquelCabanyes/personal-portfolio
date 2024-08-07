e952b158bdd356fa0dc67f970538c481961f61a1
Raquel Cabanyes <raquel.cabanyes@grupoonetec.com>
2024-08-07 12:47:01 +0200

    change styles and mark section

--- src/app/component/navbar/navbar.component.ts
+++ src/app/component/navbar/navbar.component.ts
@@ -1,23 +1,47 @@
-import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { CommonModule, Location } from '@angular/common';
+import { Component, HostListener, OnInit } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faPhone, faBars } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [FontAwesomeModule, CommonModule],
+  imports: [FontAwesomeModule, CommonModule, RouterModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss',
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
   public faPhone = faPhone;
   public faBars = faBars;
   public isActive = false;
+  public selectedSection = '/home';
+  public widthMovil = 768;
 
-  public showmenu() {
-    if(this.isActive || window.innerWidth > 768) {
-        return true;
+  @HostListener('window:resize')
+  onResize() {
+    if(window.innerWidth < this.widthMovil) {
+      this.isActive = false;
+    } else {
+      this.isActive = true;
+    }
+  }
+
+  constructor(private location: Location) {}
+
+  ngOnInit(): void {
+    this.checkSection();
+  }
+
+  public checkSection(): void {
+    this.selectedSection = this.location.path();
+  }
+
+  public showMenu() {
+    if(window.innerWidth > 768) {
+      return true;
+    } else if(this.isActive && window.innerWidth < this.widthMovil) {
+      return true;
     }
     return false;
   }
