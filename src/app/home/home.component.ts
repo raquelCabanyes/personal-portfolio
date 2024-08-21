78f3caba90ef11956899325cf1afcec4a94efb4c
Raquel Cabanyes <raquel.cabanyes@grupoonetec.com>
2024-08-21 11:57:48 +0200

    create and change styles contact page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { ContactComponent } from '../component/contact/contact.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [ContactComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
