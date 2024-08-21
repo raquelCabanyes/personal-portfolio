23f0006282806aa73a11a495fbc43c9c0fcd1deb
Raquel Cabanyes <raquel.cabanyes@grupoonetec.com>
2024-08-21 15:35:48 +0200

    Added courses, icons and animation

--- src/app/component/courses/courses.component.ts
+++ src/app/component/courses/courses.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-courses',
@@ -7,6 +7,36 @@ import { Component } from '@angular/core';
   templateUrl: './courses.component.html',
   styleUrl: './courses.component.scss'
 })
-export class CoursesComponent {
+export class CoursesComponent implements AfterViewInit {
+  @ViewChild('container', { static: false }) containerElem!: ElementRef;
 
+  private observerDiv!: IntersectionObserver;
+
+  public iconVue = '../../../assets/icons/vue.svg';
+  public iconCSS = '../../../assets/icons/css.svg';
+  public iconNgRx = '../../../assets/icons/ngrx.svg';
+  public iconLit = '../../../assets/icons/lit.svg';
+  public iconAngular = '../../../assets/icons/angular.svg';
+  public iconJest = '../../../assets/icons/jest.svg';
+  public iconReact = '../../../assets/icons/react.svg';
+  public iconPolymer = '../../../assets/icons/polymer.svg';
+
+  ngAfterViewInit() {
+    this.observerDiv = new IntersectionObserver((entries) => {
+      entries.forEach(entry => {
+        if (entry && entry.isIntersecting) {
+          const elementsToShow = Array.from(this.containerElem.nativeElement.querySelectorAll('.animated-element')).reverse() as HTMLElement[];
+          elementsToShow.forEach((element: HTMLElement, index: number) => {
+              element.classList.add('show');
+              const delay = (index + 1) * 0.3;
+              element.style.animationDelay = `${delay}s`;
+
+          });
+          this.observerDiv.unobserve(entry.target);
+        }
+      });
+    });
+
+    this.observerDiv.observe(this.containerElem.nativeElement);
+  }
 }
