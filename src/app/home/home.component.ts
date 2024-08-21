894c2d02c7a5f02d307aebf5a2e8f92aab2514c4
Raquel Cabanyes <raquel.cabanyes@grupoonetec.com>
2024-08-21 12:54:34 +0200

    Add about component to home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { ContactComponent } from '../component/contact/contact.component';
+import { AboutComponent } from '../component/about/about.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [ContactComponent],
+  imports: [ContactComponent, AboutComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
