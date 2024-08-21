c4449302699c5d4014051dd031b7f098484d38bf
Raquel Cabanyes <raquel.cabanyes@grupoonetec.com>
2024-08-21 14:53:32 +0200

    Add experience

--- src/app/component/experience/experience.component.ts
+++ src/app/component/experience/experience.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Renderer2, AfterViewInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-experience',
@@ -7,6 +7,25 @@ import { Component } from '@angular/core';
   templateUrl: './experience.component.html',
   styleUrl: './experience.component.scss'
 })
-export class ExperienceComponent {
+export class ExperienceComponent implements AfterViewInit {
+  @ViewChild('containerJobs', { static: false }) containerJobsElem!: ElementRef;
 
+  private observerResponsibilities!: IntersectionObserver;
+
+  constructor(private renderer: Renderer2) {}
+
+  ngAfterViewInit() {
+    const responsibilitiesElem = this.containerJobsElem.nativeElement.querySelectorAll('.responsibilities');
+
+    this.observerResponsibilities = new IntersectionObserver(async ([entry]) => {
+      if (entry && entry.isIntersecting) {
+        this.renderer.addClass(entry.target, 'fade-in');
+        this.observerResponsibilities.unobserve(entry.target);
+      }
+    });
+
+    responsibilitiesElem.forEach((element: Element) => {
+      this.observerResponsibilities.observe(element);
+    });
+  }
 }
