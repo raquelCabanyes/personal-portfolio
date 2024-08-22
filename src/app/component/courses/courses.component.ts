17d83fccd49157ff5961a36a765823e51323e109
Raquel Cabanyes <raquel.cabanyes@grupoonetec.com>
2024-08-22 13:44:50 +0200

    Create array with the course infor and show it in the component

--- src/app/component/courses/courses.component.ts
+++ src/app/component/courses/courses.component.ts
@@ -1,9 +1,11 @@
 import { Component, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
+import { ICourse } from '../../interfaces/course';
+import { CourseItemComponent } from '../course-item/course-item.component';
 
 @Component({
   selector: 'app-courses',
   standalone: true,
-  imports: [],
+  imports: [CourseItemComponent],
   templateUrl: './courses.component.html',
   styleUrl: './courses.component.scss',
 })
@@ -12,14 +14,90 @@ export class CoursesComponent implements AfterViewInit {
 
   private observerDiv!: IntersectionObserver;
 
-  public iconVue = '../../../assets/icons/vue.svg';
-  public iconCSS = '../../../assets/icons/css.svg';
-  public iconNgRx = '../../../assets/icons/ngrx.svg';
-  public iconLit = '../../../assets/icons/lit.svg';
-  public iconAngular = '../../../assets/icons/angular.svg';
-  public iconJest = '../../../assets/icons/jest.svg';
-  public iconReact = '../../../assets/icons/react.svg';
-  public iconPolymer = '../../../assets/icons/polymer.svg';
+  public coursesList: ICourse[] = [{
+    title: 'Vue 3',
+    year: '2023',
+    img: {
+      src: '../../../assets/icons/vue.svg',
+      alt: 'Icono de Vue'
+    }
+  },
+  {
+    title: 'Angular Universal',
+    year: '2022',
+    img: null
+  },
+  {
+    title: 'Patrones de diseño en Javascript y Node.JS',
+    year: '2022',
+    img: null
+  },
+  {
+    title: 'Curso CSS Profesional',
+    year: '2022',
+    img: {
+      src: '../../../assets/icons/css.svg',
+      alt: 'Icono de CSS'
+    }
+  },
+  {
+    title: 'Curso NgRx',
+    year: '2021',
+    img: {
+      src: '../../../assets/icons/ngrx.svg',
+      alt: 'Icono de NgRx'
+    }
+  },
+  {
+    title: 'Curso LitElement',
+    year: '2020',
+    img: {
+      src: '../../../assets/icons/lit.svg',
+      alt: 'Icono de LitElement'
+    }
+  },
+  {
+    title: 'Curso Angular 10',
+    year: '2019',
+    img: {
+      src: '../../../assets/icons/angular.svg',
+      alt: 'Icono de Angular'
+    }
+  },
+  {
+    title: 'Curso de Scrum Master',
+    year: '2018',
+    img: null
+  },
+  {
+    title: 'Curso de Testing Jest',
+    year: '2018',
+    img: {
+      src: '../../../assets/icons/jest.svg',
+      alt: 'Icono de Jest'
+    }
+  },
+  {
+    title: 'Curso introducción a Cells',
+    year: '2018',
+    img: null
+  },
+  {
+    title: 'Curso de ReactJS',
+    year: '2018',
+    img: {
+      src: '../../../assets/icons/react.svg',
+      alt: 'Icono de React'
+    }
+  },
+  {
+    title: 'Curso de Polymer',
+    year: '2017',
+    img: {
+      src: '../../../assets/icons/polymer.svg',
+      alt: 'Icono de Polymer'
+    }
+  }]
 
   ngAfterViewInit() {
     this.observerDiv = new IntersectionObserver(entries => {
